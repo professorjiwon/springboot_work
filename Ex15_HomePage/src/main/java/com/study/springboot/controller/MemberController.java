264f9ff3ad5467e01027a676e347a2694fbadd90
professorjiwon <professorjiwon@gmail.com>
2025-04-01 17:16:16 +0900

    04.01_7교시

---
+++ Ex15_HomePage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
+	
+	@GetMapping("/enrollForm")
+	public String enrollForm() {
+		return "member/enrollForm";
+	}
+
+}
