cac634f42dc430acb96de52ed8f960385e16e4db
professorjiwon <professorjiwon@gmail.com>
2025-04-01 17:45:48 +0900

    04.01_7교시

--- Ex15_HomePage/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex15_HomePage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -1,11 +1,25 @@
 package com.study.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.study.springboot.domain.Member;
+import com.study.springboot.service.MemberService;
 
 @Controller
 public class MemberController {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@Autowired
+	PasswordEncoder pEncoder;
 	
 	@RequestMapping("/")
 	public String root() {
@@ -16,5 +30,20 @@ public class MemberController {
 	public String enrollForm() {
 		return "member/enrollForm";
 	}
+	
+	@GetMapping("/idCheck")
+	public @ResponseBody boolean idCheck(@RequestParam("id") String id) {
+		return memberService.idCheck(id);
+	}
+	
+	@PostMapping("/memberInsert")
+	public String insert(Member member) {
+		String enPass = pEncoder.encode(member.getPassword());
+		member.setPassword(enPass);
+		
+		
+		return "redirect:/";
+	}
+	
 
 }
