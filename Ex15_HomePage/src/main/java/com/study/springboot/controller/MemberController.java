474ce71cf4b15cced11c3285f9c4f59dfb56f3d8
professorjiwon <professorjiwon@gmail.com>
2025-04-02 09:39:55 +0900

    04.01_8교시

--- Ex15_HomePage/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex15_HomePage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -3,16 +3,19 @@ package com.study.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.study.springboot.domain.Member;
 import com.study.springboot.service.MemberService;
 
 @Controller
+@SessionAttributes("loginUser")
 public class MemberController {
 	
 	@Autowired
@@ -37,13 +40,22 @@ public class MemberController {
 	}
 	
 	@PostMapping("/memberInsert")
-	public String insert(Member member) {
+	public String insert(Member member, Model model) {
 		String enPass = pEncoder.encode(member.getPassword());
 		member.setPassword(enPass);
+		Member m = memberService.insert(member);
 		
-		
+		model.addAttribute("loginUser",m);
 		return "redirect:/";
 	}
+	
+	/*
+	 * 로그인 시
+	   password
+	   
+	   pEncode.matches(사용자가넣은 패스워드, DB에서 가져온 패스워드)
+	   
+	 */
 	
 
 }
--- Ex15_HomePage/src/main/java/com/study/springboot/service/MemberService.java
+++ Ex15_HomePage/src/main/java/com/study/springboot/service/MemberService.java
@@ -3,6 +3,7 @@ package com.study.springboot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.study.springboot.domain.Member;
 import com.study.springboot.repository.MemberRepository;
 
 @Service
@@ -13,6 +14,10 @@ public class MemberService {
 	
 	public boolean idCheck(String id) {
 		return memberRepository.existsById(id);
+	}
+
+	public Member insert(Member member) {
+		return memberRepository.save(member);
 	}
 	
 }
