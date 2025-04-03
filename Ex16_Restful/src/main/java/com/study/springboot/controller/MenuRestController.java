6d1639d70f7f684d2e545e002dcfb222f89efc52
professorjiwon <professorjiwon@gmail.com>
2025-04-03 12:52:42 +0900

    04.03_3교시

--- Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
+++ Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.springboot.domain.Menu;
+import com.study.springboot.domain.Type;
 import com.study.springboot.service.MenuService;
 
 @RestController
@@ -21,5 +23,23 @@ public class MenuRestController {
 	public List<Menu> menuAllList() {
 		return menuService.menuAllList();
 	}
+	
+	//  /menu/type/${e.target.value}
+	//  /menu/type/KR, /menu/type/CH, /menu/type/JP
+	// {변수명} : URL의 변수로서 클라이언트가 요청할 때 경로에 포함된 값을 파라미터로 전달
+	@GetMapping("/type/{type}")
+	public List<Menu> findByType(@PathVariable(name="type") Type type) {
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
