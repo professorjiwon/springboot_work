e140c7dbd64ccc8eeee23727a2c97d85c12156e4
professorjiwon <professorjiwon@gmail.com>
2025-04-03 14:49:23 +0900

    04.03_4교시

--- Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
+++ Ex16_Restful/src/main/java/com/study/springboot/controller/MenuRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.springboot.domain.Menu;
+import com.study.springboot.domain.Taste;
 import com.study.springboot.domain.Type;
 import com.study.springboot.service.MenuService;
 
@@ -29,9 +30,21 @@ public class MenuRestController {
 	// {변수명} : URL의 변수로서 클라이언트가 요청할 때 경로에 포함된 값을 파라미터로 전달
 	@GetMapping("/type/{type}")
 	public List<Menu> findByType(@PathVariable(name="type") Type type) {
-		
+		return menuService.findByType(type);
 	}
-
+	
+	// /menu/type/${type}/taste/${taste}
+	@GetMapping("/type/{type}/taste/{taste}")
+	public List<Menu> findByTypeAndTaste(@PathVariable(name="type") Type type ,
+										 @PathVariable(name="taste") Taste taste)
+	{	
+		return menuService.findByTypeAndTaste(type, taste); 
+	}
+	
+	@GetMapping("/{id}")
+	public Menu findById(@PathVariable(name="id") Long id) {
+		return menuService.findById(id).get();
+	}
 }
 
 
--- Ex16_Restful/src/main/java/com/study/springboot/service/MenuService.java
+++ Ex16_Restful/src/main/java/com/study/springboot/service/MenuService.java
@@ -1,11 +1,14 @@
 package com.study.springboot.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.study.springboot.domain.Menu;
+import com.study.springboot.domain.Taste;
+import com.study.springboot.domain.Type;
 import com.study.springboot.repository.MenuRepository;
 
 @Service
@@ -16,6 +19,18 @@ public class MenuService {
 
 	public List<Menu> menuAllList() {
 		return menuRepository.findAll();
+	}
+
+	public List<Menu> findByType(Type type) {
+		return menuRepository.findByType(type);
+	}
+
+	public List<Menu> findByTypeAndTaste(Type type, Taste taste) {
+		return menuRepository.findByTypeAndTaste(type, taste);
+	}
+
+	public Optional<Menu> findById(Long id) {
+		return menuRepository.findById(id);
 	}
 
 }
